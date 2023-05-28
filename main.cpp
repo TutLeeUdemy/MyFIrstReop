e32966e39e76514f5da3e4f99d72c4491b815564
JTLee <jtleetut@gmail.com>
2023-05-28 10:41:08 -0500

    updated line 11

--- main.cpp
+++ main.cpp
@@ -8,6 +8,6 @@ int main()
 	std::cout << "hello world!\n";
 	std::cout << "hello world!\n";
 	std::cout << "hello world!I love this line\n";
-	std::cout << "hello world! - super cool line, needed to have it\n";
+	std::cout << "super cool line, need to keep it\n";
 	std::cout << "I added a new one" << std::endl;
 }
